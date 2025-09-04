47e10a1df7ab1482dc6a7d26d07c402440784ee6
ivoryzhangwy <ivoryzhangwy@gmail.com>
2025-09-04 16:40:55 -0700

    fix socketio

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ import os
 if __name__ == "__main__":
     import ivoryos
     from ivoryos.config import DemoConfig
+    import eventlet
+
+    eventlet.monkey_patch()
+
     _config = DemoConfig()
     _config.SESSION_COOKIE_SAMESITE = "Lax"
-    ivoryos.run(__name__, config=DemoConfig(), blueprint_plugins=source_code, port=7860)
+
+    port = int(os.environ.get("PORT", 7860))
+
+    ivoryos.run(__name__, config=DemoConfig(), blueprint_plugins=source_code, port=port)
 
     # # USE CASE 2 - start OS using current module and enable LLM with Ollama
     # ivoryos.run(__name__, model="llama3.1", llm_server='localhost')
